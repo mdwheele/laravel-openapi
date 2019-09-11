5e7108f7cfb5c627a733d9b993b45b28adffe5e2
Dustin Wheeler <mdwheele@ncsu.edu>
2019-09-11 19:14:21 +0000

    Add endpoint to exercise parameter specification.

--- tests/Controllers/CoreFeaturesController.php
+++ tests/Controllers/CoreFeaturesController.php
@@ -3,6 +3,7 @@
 namespace Mdwheele\OpenApi\Tests\Controllers;
 
 use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 class CoreFeaturesController extends Controller
 {
@@ -16,6 +17,19 @@ class CoreFeaturesController extends Controller
                     'timestamp' => Carbon::now()->toRfc3339String()
                 ]
             ]
+        ];
+    }
+
+    public function parameters(Request $request, $email)
+    {
+        return [
+            'message' => "Hey {$email}! Looks like you're filtering by {$request->query('filter')} and love {$request->header('X-Super-Hero')}.",
+            'path' => [
+                'email' => $email
+            ],
+            'query' => $request->query(),
+            'header' => $request->headers->all(),
+            'cookie' => $request->cookies->all()
         ];
     }
 }
